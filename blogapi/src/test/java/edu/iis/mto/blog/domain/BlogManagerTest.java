7070235e868341772d4e93734b7bc23b291e34d4
Barabasz.Rafal <215671@edu.p.lodz.pl>
2020-05-25 17:36:13 +0200

    3.2 Updated BlogManager, DomainError and added new test cases connected with this changes related to liking posts by unconfirmed users.

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,24 +1,30 @@
 package edu.iis.mto.blog.domain;
 
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.verify;
-
+import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
+import edu.iis.mto.blog.domain.repository.UserRepository;
+import edu.iis.mto.blog.mapper.BlogDataMapper;
+import edu.iis.mto.blog.services.BlogService;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import edu.iis.mto.blog.api.request.UserRequest;
-import edu.iis.mto.blog.domain.model.AccountStatus;
-import edu.iis.mto.blog.domain.model.User;
-import edu.iis.mto.blog.domain.repository.UserRepository;
-import edu.iis.mto.blog.mapper.BlogDataMapper;
-import edu.iis.mto.blog.services.BlogService;
+import java.util.Optional;
+
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,6 +32,10 @@ public class BlogManagerTest {
 
     @MockBean
     private UserRepository userRepository;
+    @MockBean
+    private BlogPostRepository blogPostRepository;
+    @MockBean
+    private LikePostRepository likedPostRepository;
 
     @Autowired
     private BlogDataMapper dataMapper;
@@ -44,4 +54,26 @@ public class BlogManagerTest {
         assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
+    @Test(expected = DomainError.class)
+    public void shouldThrowDomainErrorWhenLikeComesFromNewUser(){
+        User user = new User();
+        user.setAccountStatus(AccountStatus.CONFIRMED);
+        user.setId(1L);
+
+        User fan = new User();
+        fan.setAccountStatus(AccountStatus.NEW);
+        fan.setId(2L);
+
+        BlogPost blogPost = new BlogPost();
+        blogPost.setUser(user);
+        blogPost.setEntry("ExampleData");
+        blogPost.setId(10L);
+
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.ofNullable(user));
+        Mockito.when(userRepository.findById(2L)).thenReturn(Optional.ofNullable(fan));
+        Mockito.when(blogPostRepository.findById(blogPost.getId())).thenReturn(Optional.ofNullable(blogPost));
+        Mockito.when(likedPostRepository.findByUserAndPost(user, blogPost)).thenReturn(Optional.empty());
+
+        blogService.addLikeToPost(fan.getId(), blogPost.getId());
+    }
 }
