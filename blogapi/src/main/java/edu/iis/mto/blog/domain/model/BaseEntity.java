fa7b794e519a220f74e64fa3d669708f5b9d7ccd
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-05-27 14:51:53 +0200

    h2 base dont work with identity strategy

--- blogapi/src/main/java/edu/iis/mto/blog/domain/model/BaseEntity.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/model/BaseEntity.java
@@ -13,7 +13,7 @@ public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO) //baza h2 nie wspiera strategii IDENTITY
     private Long id;
 
     public Long getId() {
