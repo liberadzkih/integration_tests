1a204f18cb5da627c51482c06a213196fe1bcb2b
Dawid <215920@edu.p.lodz.pl>
2020-06-01 14:46:43 +0200

    like test implemented

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.blog.rest.test;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
@@ -21,21 +22,21 @@ public class CreateUserTest extends FunctionalTests {
     public void createUserWithProperDataReturnsCreatedStatus() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy1@domain.com");
         given().accept(ContentType.JSON)
-               .header("Content-Type", "application/json;charset=UTF-8")
-               .body(jsonObj.toString())
-               .expect()
-               .log()
-               .all()
-               .statusCode(HttpStatus.SC_CREATED)
-               .when()
-               .post(USER_API);
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CREATED)
+                .when()
+                .post(USER_API);
     }
 
     @Test
-    public void shouldNotAllowUserCreationWithOccupiedEmail(){
+    public void shouldNotAllowUserCreationWithOccupiedEmail() {
         JSONObject jsonObject = new JSONObject()
-                .put("email","215920@edu.p.lodz.pl")
-                .put("firstName","Dawid")
+                .put("email", "215920@edu.p.lodz.pl")
+                .put("firstName", "Dawid")
                 .put("lastName", "Witaszek");
 
         given().accept(ContentType.JSON)
@@ -50,7 +51,7 @@ public class CreateUserTest extends FunctionalTests {
     }
 
     @Test
-    public void shouldAllowCreatingPostByConfirmedUser(){
+    public void shouldAllowCreatingPostByConfirmedUser() {
         JSONObject jsonObject = new JSONObject()
                 .put("entry", "DEADBEEF");
 
@@ -67,8 +68,8 @@ public class CreateUserTest extends FunctionalTests {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {2,3})
-    public void shouldNotAllwoCreatingPostByUnconfirmedUser(int userId){
+    @ValueSource(ints = {2, 3})
+    public void shouldNotAllwoCreatingPostByUnconfirmedUser(int userId) {
         JSONObject jsonObject = new JSONObject()
                 .put("entry", "DEADBEEF");
 
@@ -83,4 +84,89 @@ public class CreateUserTest extends FunctionalTests {
                 .when()
                 .post(USER_API + "/" + userId + "/post");
     }
+
+    @Test
+    public void userLikePost() {
+        JSONObject jsonObject = new JSONObject();
+
+        given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObject.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post(USER_API + "/1/like/1");
+    }
+
+    @Test
+    public void userCannotAddLikeToHisOwnPost() {
+        JSONObject jsonObject = new JSONObject();
+
+        given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObject.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(USER_API + "/2/like/1");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3})
+    public void unconfirmedUserCannotAddLike(int userId) {
+        JSONObject jsonObject = new JSONObject();
+
+        given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObject.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(USER_API + "/" + userId + "/like/1");
+    }
+
+    @Test
+    public void cannotLikeTwoTimes() {
+        JSONObject jsonObject = new JSONObject();
+
+        given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObject.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post(USER_API + "/1/like/1");
+
+        String string = given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObject.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post(USER_API + "/1/like/1")
+                .then()
+                .assertThat()
+                .extract()
+                .asString();
+
+        assertEquals("false", string);
+    }
+
+
+
 }
