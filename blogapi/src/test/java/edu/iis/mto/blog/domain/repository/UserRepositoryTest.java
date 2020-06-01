d7d2284579b6029687643f1fbc9a4f1b0cf8d18a
AndrzejBilant <andrzej.bilant@gmail.com>
2020-06-01 14:10:52 +0200

    Added test for UserRepository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase method
    Signed-off-by: AndrzejBilant <andrzej.bilant@gmail.com>

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -14,52 +14,216 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepositoryTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
+	@Autowired
+	private TestEntityManager entityManager;
 
-    @Autowired
-    private UserRepository repository;
+	@Autowired
+	private UserRepository repository;
 
-    private User user;
+	private User user;
 
-    @Before
-    public void setUp() {
-        user = new User();
-        user.setFirstName("Jan");
-        user.setEmail("john@domain.com");
-        user.setAccountStatus(AccountStatus.NEW);
-    }
+	@Before
+	public void setUp() {
+		user = new User();
+		user.setFirstName("Jan");
+		user.setEmail("john@domain.com");
+		user.setAccountStatus(AccountStatus.NEW);
+	}
 
-    @Test
-    public void shouldFindNoUsersIfRepositoryIsEmpty() {
+	@Test
+	public void shouldFindNoUsersIfRepositoryIsEmpty() {
 
-        List<User> users = repository.findAll();
+		List<User> users = repository.findAll();
 
-        assertThat(users, hasSize(0));
-    }
+		assertThat(users, hasSize(0));
+	}
 
-    @Test
-    public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
-        User persistedUser = entityManager.persist(user);
-        List<User> users = repository.findAll();
+	@Test
+	public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
+		User persistedUser = entityManager.persist(user);
+		List<User> users = repository.findAll();
 
-        assertThat(users, hasSize(1));
-        assertThat(users.get(0)
-                        .getEmail(),
-                equalTo(persistedUser.getEmail()));
-    }
+		assertThat(users, hasSize(1));
+		assertThat(users.get(0)
+						.getEmail(),
+				equalTo(persistedUser.getEmail()));
+	}
 
-    @Test
-    public void shouldStoreANewUser() {
+	@Test
+	public void shouldStoreANewUser() {
+		User persistedUser = repository.save(user);
 
-        User persistedUser = repository.save(user);
+		assertThat(persistedUser.getId(), notNullValue());
+	}
 
-        assertThat(persistedUser.getId(), notNullValue());
-    }
+	@Test
+	public void shouldFindOneRecordFoundByFirstNameListHasOneElement() {
+		repository.save(user);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						user.getFirstName(), "X", "X"));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0), equalTo(user));
+	}
 
+	@Test
+	public void shouldFindOneRecordFoundByLastNameListHasOneElement() {
+		user.setLastName("Bilant");
+		repository.save(user);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						"X", user.getLastName(), "X"));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0), equalTo(user));
+	}
+
+	@Test
+	public void shouldFindOneRecordFoundByEmailListHasOneElement() {
+		repository.save(user);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						"X", "X", user.getEmail()));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0), equalTo(user));
+	}
+
+	@Test
+	public void shouldFindOneRecordFoundByFirstNameListHasThreeElements() {
+		repository.save(user);
+		User user2 = new User();
+		user2.setLastName("Bilant");
+		user2.setEmail("andrzej@domain.pl");
+		user2.setFirstName("Andrzej");
+		user2.setAccountStatus(AccountStatus.NEW);
+		repository.save(user2);
+		User user3 = new User();
+		user3.setLastName("Potter");
+		user3.setEmail("harry@domain.pl");
+		user3.setFirstName("Harry");
+		user3.setAccountStatus(AccountStatus.NEW);
+		repository.save(user3);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						user.getFirstName(), "X", "X"));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0), equalTo(user));
+	}
+
+	@Test
+	public void shouldFindOneRecordFoundByLastNameListHasThreeElements() {
+		user.setLastName("Bilant");
+		repository.save(user);
+		User user2 = new User();
+		user2.setLastName("Dokimuk");
+		user2.setEmail("andrzej@domain.pl");
+		user2.setFirstName("Andrzej");
+		user2.setAccountStatus(AccountStatus.NEW);
+		repository.save(user2);
+		User user3 = new User();
+		user3.setLastName("Potter");
+		user3.setEmail("harry@domain.pl");
+		user3.setFirstName("Harry");
+		user3.setAccountStatus(AccountStatus.NEW);
+		repository.save(user3);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						"X", user.getLastName(), "X"));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0), equalTo(user));
+	}
+
+	@Test
+	public void shouldFindOneRecordFoundByEmailListHasThreeElements() {
+		repository.save(user);
+		User user2 = new User();
+		user2.setLastName("Dokimuk");
+		user2.setEmail("andrzej@domain.pl");
+		user2.setFirstName("Andrzej");
+		user2.setAccountStatus(AccountStatus.NEW);
+		repository.save(user2);
+		User user3 = new User();
+		user3.setLastName("Potter");
+		user3.setEmail("harry@domain.pl");
+		user3.setFirstName("Harry");
+		user3.setAccountStatus(AccountStatus.NEW);
+		repository.save(user3);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						"X", "X", user.getEmail().toLowerCase()));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0), equalTo(user));
+	}
+
+	@Test
+	public void shouldFindTwoRecordsFoundByLastNameListHasTwoElements() {
+		user.setLastName("Bilant");
+		repository.save(user);
+		User user2 = new User();
+		user2.setLastName("Bilant");
+		user2.setEmail("andrzej@domain.pl");
+		user2.setFirstName("Andrzej");
+		user2.setAccountStatus(AccountStatus.NEW);
+		repository.save(user2);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						"X", user.getLastName().toUpperCase(), "X"));
+		assertThat(result, hasSize(2));
+		assertTrue(result.contains(user));
+		assertTrue(result.contains(user2));
+	}
+
+	@Test
+	public void shouldFindTwoRecordsListHasThreeElements() {
+		user.setLastName("Bilant");
+		repository.save(user);
+		User user2 = new User();
+		user2.setLastName("Dokimuk");
+		user2.setEmail("andrzej@domain.pl");
+		user2.setFirstName("Andrzej");
+		user2.setAccountStatus(AccountStatus.NEW);
+		repository.save(user2);
+		User user3 = new User();
+		user3.setLastName("Potter");
+		user3.setEmail("harry@domain.pl");
+		user3.setFirstName("Harry");
+		user3.setAccountStatus(AccountStatus.NEW);
+		repository.save(user3);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						user2.getFirstName(), user.getLastName(), "X"));
+		assertThat(result, hasSize(2));
+		assertTrue(result.contains(user));
+		assertTrue(result.contains(user2));
+	}
+
+	@Test
+	public void shouldFindThreeRecordsListHasThreeElements() {
+		user.setLastName("Bilant");
+		repository.save(user);
+		User user2 = new User();
+		user2.setLastName("Dokimuk");
+		user2.setEmail("andrzej@domain.pl");
+		user2.setFirstName("Andrzej");
+		user2.setAccountStatus(AccountStatus.NEW);
+		repository.save(user2);
+		User user3 = new User();
+		user3.setLastName("Potter");
+		user3.setEmail("harry@domain.pl");
+		user3.setFirstName("Harry");
+		user3.setAccountStatus(AccountStatus.NEW);
+		repository.save(user3);
+		List<User> result = (repository.
+				findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+						user2.getFirstName(), user.getLastName(), user3.getEmail()));
+		assertThat(result, hasSize(3));
+		assertTrue(result.contains(user));
+		assertTrue(result.contains(user2));
+		assertTrue(result.contains(user3));
+	}
 }
