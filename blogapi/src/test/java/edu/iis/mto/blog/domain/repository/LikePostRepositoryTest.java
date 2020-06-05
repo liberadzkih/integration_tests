af6ee69eadfe7298e19f9c0d96684d8469063e1b
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-06-05 17:59:27 +0200

    3.1 Added test for LikePostRepositoryTest

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -13,8 +13,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -22,9 +23,6 @@ import static org.junit.Assert.assertThat;
 public class LikePostRepositoryTest {
     @Autowired
     private TestEntityManager entityManager;
-
-    @Autowired
-    private UserRepository repository;
 
     @Autowired
     private LikePostRepository likePostRepository;
@@ -48,5 +46,69 @@ public class LikePostRepositoryTest {
         likePost = new LikePost();
         likePost.setUser(user);
         likePost.setPost(blogPost);
+
+        entityManager.persist(user);
+        entityManager.persist(blogPost);
+    }
+
+    @Test
+    public void shouldFindNoLikePostIfLikePostRepositoryIsEmpty() {
+
+        List<LikePost> likePostList = likePostRepository.findAll();
+
+        assertThat(likePostList, hasSize(0));
+    }
+
+    @Test
+    public void shouldFindOneLikePostIfLikePostRepositoryContainsOneLikePostEntity() {
+
+        LikePost persistedLikePost = entityManager.persist(likePost);
+        List<LikePost> likePostList = likePostRepository.findAll();
+
+        assertThat(likePostList, hasSize(1));
+        assertThat(likePostList.get(0)
+                        .getPost(),
+                equalTo(persistedLikePost.getPost()));
+    }
+
+    @Test
+    public void shouldStoreANewLikePost() {
+        LikePost persistedLikePost = likePostRepository.save(likePost);
+
+        assertThat(persistedLikePost.getId(), notNullValue());
+    }
+
+    @Test
+    public void shouldFindLikePostWhenUserAndBLogPostAreCorrect() {
+        likePostRepository.save(likePost);
+        Optional<LikePost> likePostOptional = likePostRepository.findByUserAndPost(user, blogPost);
+
+        assertThat(likePostOptional.get(), is(likePost));
+    }
+
+    @Test
+    public void shouldNotFindLikePostWhenBlogPostIsIncorrect() {
+        BlogPost otherBlogPost = new BlogPost();
+        otherBlogPost.setEntry("Other post");
+        otherBlogPost.setUser(user);
+        entityManager.persist(otherBlogPost);
+        likePostRepository.save(likePost);
+        Optional<LikePost> likePostOptional = likePostRepository.findByUserAndPost(user, otherBlogPost);
+
+        assertThat(likePostOptional, is(Optional.empty()));
+    }
+
+    @Test
+    public void shouldNotFindLikePostWhenUserIsIncorrect() {
+        User otherUser = new User();
+        otherUser.setFirstName("Matthew");
+        otherUser.setLastName("Noname");
+        otherUser.setEmail("noname@gmail.com");
+        otherUser.setAccountStatus(AccountStatus.NEW);
+        entityManager.persist(otherUser);
+        likePostRepository.save(likePost);
+        Optional<LikePost> likePostOptional = likePostRepository.findByUserAndPost(otherUser, blogPost);
+
+        assertThat(likePostOptional, is(Optional.empty()));
     }
 }
