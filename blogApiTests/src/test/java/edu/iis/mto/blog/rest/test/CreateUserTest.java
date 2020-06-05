a6ff5f4873111f6be628e9263a07b7a827d24ff8
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-06-05 23:29:21 +0200

    3.4.2 Created tests which check if CONFIRMED user add post

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.port;
 
 public class CreateUserTest extends FunctionalTests {
 
@@ -54,5 +55,35 @@ public class CreateUserTest extends FunctionalTests {
                 .statusCode(HttpStatus.SC_CONFLICT)
                 .when()
                 .post(USER_API);
+    }
+
+    @Test
+    public void addPostByUserOnlyCONFIRMEDUserCanAddPost() {
+        String id = "1";
+        JSONObject jsonObject = new JSONObject().put("entry", "Some text");
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObject.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CREATED)
+                .when()
+                .post(USER_API + "/" + id + "/post");
+    }
+
+    @Test
+    public void addPostByUserNewUserCannotAddPost() {
+        String id = "3";
+        JSONObject jsonObject = new JSONObject().put("entry", "Some text");
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObject.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(USER_API + "/" + id + "/post");
     }
 }
