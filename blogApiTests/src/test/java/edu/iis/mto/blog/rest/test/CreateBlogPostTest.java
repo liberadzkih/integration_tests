7269fba1df6445ebf7ca6516c136c97d8d1e2dc2
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-06-09 21:21:58 +0200

    one more attempt to moving with integration tests...

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateBlogPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateBlogPostTest.java
@@ -9,5 +9,47 @@ import static io.restassured.RestAssured.given;
 
 public class CreateBlogPostTest  extends FunctionalTests {
 
+//tylko uzytkownik ze statusem CONFIRMED moze dodac post
 
+    @Test
+    public void createBlogPostByConfirmedUserResultWithSuccess() {
+        JSONObject jsonObj = new JSONObject().put("entry", "xxxxx");
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .body(jsonObj.toString())
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_CREATED)
+               .when()
+               .post("/blog/user/1/post");
+    }
+
+    @Test
+    public void createBlogPostByNotExistingUserResultWithBadRequestStatus() {
+        JSONObject jsonObj = new JSONObject().put("entry", "xxxxx");
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .body(jsonObj.toString())
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post("/blog/user/53/post");
+    }
+
+    @Test
+    public void createBlogPostByNotConfirmedUserResultWithBadRequestStatus() {
+        JSONObject jsonObj = new JSONObject().put("entry", "xxxxx");
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .body(jsonObj.toString())
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post("/blog/user/2/post");
+    }
 }
