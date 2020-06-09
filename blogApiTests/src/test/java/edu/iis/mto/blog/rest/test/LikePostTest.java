7e149fd594980303279b515399b726d1da9d2fa6
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-06-09 21:36:42 +0200

    not confirmed user cant like blog post

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -1,5 +1,26 @@
 package edu.iis.mto.blog.rest.test;
 
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class LikePostTest extends FunctionalTests {
+
+    @Test
+    public void likingBlogPostByNotConfirmedUserResultWithBadRequst() {
+        JSONObject jsonObj = new JSONObject().put("entry", "xxxxx");
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .body(jsonObj.toString())
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post("/blog/user/2/like/1");
+    }
 
 }
