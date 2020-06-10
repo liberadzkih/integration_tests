63e5faa07cd8e1dc882ab902d41b6cf7b2f7624c
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-06-10 09:37:36 +0200

    searching users by email should find one user

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUserTest.java
@@ -9,7 +9,7 @@ import static org.hamcrest.core.Is.is;
 
 public class SearchUserTest extends FunctionalTests {
     @Test
-    public void searchingUsersShouldFindOnlyNotRemovedUsers() {
+    public void searchingUsersByFirstNameShouldFindOnlyNotRemovedUsers() {
         given().accept(ContentType.JSON)
                .header("Content-Type", "application/json;charset=UTF-8")
                .expect()
@@ -20,5 +20,19 @@ public class SearchUserTest extends FunctionalTests {
                .body("size",is(2))
                .when()
                .get("/blog/user/find?searchString=Laura");
+    }
+
+    @Test
+    public void searchingUsersByEmailShouldFindOnlyOneUser() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_OK)
+               .and()
+               .body("size",is(1))
+               .when()
+               .get("/blog/user/find?searchString=dddd@domain.com");
     }
 }
