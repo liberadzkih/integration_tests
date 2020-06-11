e36e7d8beeddba74a246e991dd785051107ec01a
Kamil_Zielinski <215933@edu.p.lodz.pl>
2020-06-11 21:50:27 +0200

    Rest API tesats

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -1,47 +1,52 @@
 package edu.iis.mto.blog.api;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.dto.Id;
+import edu.iis.mto.blog.services.BlogService;
+import edu.iis.mto.blog.services.DataFinder;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.persistence.EntityNotFoundException;
 
-import edu.iis.mto.blog.api.request.UserRequest;
-import edu.iis.mto.blog.dto.Id;
-import edu.iis.mto.blog.services.BlogService;
-import edu.iis.mto.blog.services.DataFinder;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BlogApi.class)
 public class BlogApiTest {
 
+    private UserRequest user = new UserRequest();
     @Autowired
     private MockMvc mvc;
-
     @MockBean
     private BlogService blogService;
-
     @MockBean
     private DataFinder finder;
+
+    @Before
+    public void setUp() {
+        user.setEmail("john@domain.com");
+        user.setFirstName("John");
+        user.setLastName("Steward");
+    }
 
     @Test
     public void postBlogUserShouldResponseWithStatusCreatedAndNewUserId() throws Exception {
         Long newUserId = 1L;
-        UserRequest user = new UserRequest();
-        user.setEmail("john@domain.com");
-        user.setFirstName("John");
-        user.setLastName("Steward");
         when(blogService.createUser(user)).thenReturn(newUserId);
         String content = writeJson(user);
 
@@ -52,9 +57,27 @@ public class BlogApiTest {
            .andExpect(content().string(writeJson(new Id(newUserId))));
     }
 
+    @Test
+    public void shouldThrowDataIntegrityViolationExceptionWhileCreatingWrongUser() throws Exception {
+        when(blogService.createUser(user)).thenThrow(DataIntegrityViolationException.class);
+        String content = writeJson(user);
+
+        mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON)
+                                      .accept(MediaType.APPLICATION_JSON)
+                                      .content(content))
+           .andExpect(status().isConflict());
+    }
+
     private String writeJson(Object obj) throws JsonProcessingException {
         return new ObjectMapper().writer()
                                  .writeValueAsString(obj);
     }
 
+    @Test
+    public void shouldReturn404WhenNoDataExist() throws Exception {
+        Long notExistingID = 99L;
+        when(finder.getUserData(notExistingID)).thenThrow(EntityNotFoundException.class);
+        mvc.perform(get("/blog/user{id}", notExistingID))
+           .andExpect(status().isNotFound());
+    }
 }
