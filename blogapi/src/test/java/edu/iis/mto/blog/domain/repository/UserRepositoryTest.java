6aa6a42b30e112b31bfa65673ce4d3bedab3591e
Lis-Marciniak.Jędrzej <215799@edu.p.lodz.pl>
2020-06-12 23:21:03 +0200

    [3.1.2] User builder added && user search function tests

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -26,13 +26,11 @@ public class UserRepositoryTest {
     private UserRepository repository;
 
     private User user;
+    private String anythingOtherThanWhatAnyOfTheValuesContains = "================================";
 
     @Before
     public void setUp() {
-        user = new User();
-        user.setFirstName("Jan");
-        user.setEmail("john@domain.com");
-        user.setAccountStatus(AccountStatus.NEW);
+        user = new UserBuilder().withFirstName("Jan").withEmail("john@domain.com").build();
     }
 
     @Test
@@ -60,6 +58,89 @@ public class UserRepositoryTest {
         User persistedUser = repository.save(user);
 
         assertThat(persistedUser.getId(), notNullValue());
+    }
+
+    @Test
+    public void shouldFindAllUsersWithSameName() {
+        repository.save(new UserBuilder().withFirstName("Jan").withLastName("Testowy").withEmail("student@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Janek").withLastName("Bananowy").withEmail("banan@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Jan").withLastName("Jabłkowy").withEmail("jabłko@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Dzban").withLastName("Pomarańczowy").withEmail("pomarańcza@p.lodz.pl").build());
+        List<User> foundUsersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("Jan", anythingOtherThanWhatAnyOfTheValuesContains, anythingOtherThanWhatAnyOfTheValuesContains);
+        assertThat(foundUsersList, hasSize(3));
+    }
+
+    @Test
+    public void shouldFindAllUsersWithSameLastName() {
+        repository.save(new UserBuilder().withFirstName("Jabłko").withLastName("Student").withEmail("jabłko@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Banan").withLastName("Studentowy").withEmail("banan@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Szaman").withLastName("Szamanowy").withEmail("szaman@p.lodz.pl").build());
+        List<User> foundUsersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(anythingOtherThanWhatAnyOfTheValuesContains, "Student", anythingOtherThanWhatAnyOfTheValuesContains);
+        assertThat(foundUsersList, hasSize(2));
+    }
+
+    @Test
+    public void shouldFindAllUsersWithSameEmail() {
+        repository.save(new UserBuilder().withFirstName("Biała").withLastName("Owca").withEmail("owce@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Zielona").withLastName("Owca1").withEmail("owce1@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Żółta").withLastName("Owca2").withEmail("owce2@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Czarna").withLastName("Owca4").withEmail("nieztejrodziny@p.lodz.pl").build());
+        List<User> foundUsersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(anythingOtherThanWhatAnyOfTheValuesContains, anythingOtherThanWhatAnyOfTheValuesContains, "owce");
+        assertThat(foundUsersList, hasSize(3));
+    }
+
+    @Test
+    public void shouldFindNone() {
+        repository.save(new UserBuilder().withFirstName("Jan").withLastName("Testowy").withEmail("student@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Janek").withLastName("Bananowy").withEmail("banan@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Jan").withLastName("Jabłkowy").withEmail("jabłko@p.lodz.pl").build());
+        repository.save(new UserBuilder().withFirstName("Dzban").withLastName("Pomarańczowy").withEmail("pomarańcza@p.lodz.pl").build());
+        List<User> foundUsersList = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(anythingOtherThanWhatAnyOfTheValuesContains, anythingOtherThanWhatAnyOfTheValuesContains, anythingOtherThanWhatAnyOfTheValuesContains);
+        assertThat(foundUsersList, hasSize(0));
+    }
+
+    // USER BUILDER
+    private class UserBuilder {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private AccountStatus accountStatus;
+
+        public UserBuilder() {
+            firstName = "";
+            lastName = "";
+            email = "";
+            accountStatus = AccountStatus.NEW;
+        }
+
+        public UserBuilder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder withAccountStatus(AccountStatus accountStatus) {
+            this.accountStatus = accountStatus;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.setFirstName(this.firstName);
+            user.setLastName(this.lastName);
+            user.setEmail(this.email);
+            user.setAccountStatus(this.accountStatus);
+            return user;
+        }
     }
 
 }
