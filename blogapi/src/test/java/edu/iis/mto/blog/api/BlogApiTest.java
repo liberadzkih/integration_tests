5974f5b645ae7f7727ae1d9fac9e327f009adea1
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-06-21 18:25:30 +0200

    Testy warstwy REST API - wyjątek  DataIntegrityViolationException

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -55,6 +56,26 @@ public class BlogApiTest {
     private String writeJson(Object obj) throws JsonProcessingException {
         return new ObjectMapper().writer()
                                  .writeValueAsString(obj);
+    }
+
+    @Test
+    public void postBlogUserShouldResponseWithStatusConflictWhenDataIntegrityExceptionThrown() throws Exception {
+        UserRequest user = getExampleUserRequest();
+        when(blogService.createUser(user)).thenThrow(DataIntegrityViolationException.class);
+        String content = writeJson(user);
+
+        mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON)
+                                      .accept(MediaType.APPLICATION_JSON)
+                                      .content(content))
+           .andExpect(status().isConflict());
+    }
+
+    UserRequest getExampleUserRequest(){
+        UserRequest user = new UserRequest();
+        user.setEmail("john@domain.com");
+        user.setFirstName("John");
+        user.setLastName("Steward");
+        return user;
     }
 
 }
