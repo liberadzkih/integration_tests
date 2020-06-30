5783d799a6222208d904e0cda825a8d3533a8adf
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-06-30 13:02:20 +0200

    3.4.3 post liking logic tested and added 2 blog_posts to sql database for easier testing
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 --this script initiates db for h2 db (used in test profile)
 insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian')
+insert into blog_post (id, entry, user_id) values (1, 'entry', 1)
+insert into blog_post (id, entry, user_id) values (2, 'entry', 2)
