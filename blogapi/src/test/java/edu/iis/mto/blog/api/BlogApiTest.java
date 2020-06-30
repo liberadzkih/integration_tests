cf63054f13e4b6e648f8f26820af68ae6d1d593e
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-06-30 11:51:10 +0200

    3.3.1 DataIntegrityViloation test added and minor refactoring
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -5,11 +5,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,27 +36,44 @@ public class BlogApiTest {
 
     @MockBean
     private DataFinder finder;
+    private UserRequest user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new UserRequest();
+        user.setEmail("john@domain.com");
+        user.setFirstName("John");
+        user.setLastName("Steward");
+    }
 
     @Test
     public void postBlogUserShouldResponseWithStatusCreatedAndNewUserId() throws Exception {
         Long newUserId = 1L;
-        UserRequest user = new UserRequest();
-        user.setEmail("john@domain.com");
-        user.setFirstName("John");
-        user.setLastName("Steward");
+
         when(blogService.createUser(user)).thenReturn(newUserId);
         String content = writeJson(user);
 
         mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON)
-                                      .accept(MediaType.APPLICATION_JSON)
-                                      .content(content))
-           .andExpect(status().isCreated())
-           .andExpect(content().string(writeJson(new Id(newUserId))));
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isCreated())
+                .andExpect(content().string(writeJson(new Id(newUserId))));
     }
 
     private String writeJson(Object obj) throws JsonProcessingException {
         return new ObjectMapper().writer()
-                                 .writeValueAsString(obj);
+                .writeValueAsString(obj);
+    }
+
+
+    @Test
+    public void shouldThrowDataIntegrityViolationException() throws Exception {
+        when(blogService.createUser(user)).thenThrow(DataIntegrityViolationException.class);
+        String content = writeJson(user);
+        mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isConflict());
     }
 
 }
