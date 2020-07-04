8f742d8a0d80708976b37c9674e1d7f4878271aa
debek.kacper <215702@edu.p.lodz.pl>
2020-07-04 17:04:44 +0200

    Tests associated with finding users posts

--- blogapi/src/main/java/edu/iis/mto/blog/domain/errors/DomainError.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/errors/DomainError.java
@@ -3,6 +3,7 @@ package edu.iis.mto.blog.domain.errors;
 public class DomainError extends RuntimeException {
 
     public static final String INVALID_USER_STATUS = "user not confirmed";
+    public static final String USER_REMOVED = "user has been removed";
     private static final long serialVersionUID = 1L;
 
     public static final String USER_NOT_FOUND = "unknown user";
