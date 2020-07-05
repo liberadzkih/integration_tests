67753cc12297202d4bc467b337ab118d69e79c79
Hubert Salamaga <215871@edu.p.lodz.pl>
2020-07-05 23:23:48 +0200

    Add spec to CreateUserTest that checks status when user does not have unique email

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -25,4 +25,18 @@ public class CreateUserTest extends FunctionalTests {
                .when()
                .post(USER_API);
     }
+
+    @Test
+    public void createUserWithNotUniqueEmailReturnsConflictStatus() {
+        JSONObject jsonObj = new JSONObject().put("email", "tracy1@domain.com");
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .body(jsonObj.toString())
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_CONFLICT)
+               .when()
+               .post(USER_API);
+    }
 }
