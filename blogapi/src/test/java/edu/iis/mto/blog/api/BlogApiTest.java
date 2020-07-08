c515686227f577665d16c2a681952429e2ded96b
bakpawel <bakpawel103@gmail.com>
2020-07-08 15:09:12 +0200

    Added Rest Api tests

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -1,15 +1,18 @@
 package edu.iis.mto.blog.api;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +24,8 @@ import edu.iis.mto.blog.api.request.UserRequest;
 import edu.iis.mto.blog.dto.Id;
 import edu.iis.mto.blog.services.BlogService;
 import edu.iis.mto.blog.services.DataFinder;
+
+import javax.persistence.EntityNotFoundException;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BlogApi.class)
@@ -35,13 +40,18 @@ public class BlogApiTest {
     @MockBean
     private DataFinder finder;
 
-    @Test
-    public void postBlogUserShouldResponseWithStatusCreatedAndNewUserId() throws Exception {
-        Long newUserId = 1L;
-        UserRequest user = new UserRequest();
+    private UserRequest user = new UserRequest();
+
+    @Before
+    public void setUp() throws Exception {
         user.setEmail("john@domain.com");
         user.setFirstName("John");
         user.setLastName("Steward");
+    }
+
+    @Test
+    public void responseCreatedStatusAndUserId() throws Exception {
+        Long newUserId = 1L;
         when(blogService.createUser(user)).thenReturn(newUserId);
         String content = writeJson(user);
 
@@ -52,9 +62,28 @@ public class BlogApiTest {
            .andExpect(content().string(writeJson(new Id(newUserId))));
     }
 
+    @Test
+    public void throwDataIntegrityViolationIncorrectUser() throws Exception {
+        when(blogService.createUser(user)).thenThrow(DataIntegrityViolationException.class);
+        String content = writeJson(user);
+
+        mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void returnResponse404WhenNoData() throws Exception {
+        Long notExistingID = 52L;
+        when(finder.getUserData(notExistingID)).thenThrow(EntityNotFoundException.class);
+        mvc.perform(get("/blog/user{id}", notExistingID))
+                .andExpect(status().isNotFound());
+    }
+
     private String writeJson(Object obj) throws JsonProcessingException {
         return new ObjectMapper().writer()
-                                 .writeValueAsString(obj);
+                .writeValueAsString(obj);
     }
 
 }
