a965fd98fad22371e63db981d4fe78682bb9507a
bakpawel <bakpawel103@gmail.com>
2020-07-08 11:46:12 +0200

    Added tests to UserRepositoryTest

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -18,9 +18,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.User;
 
+import static org.hamcrest.Matchers.*;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepositoryTest {
+    public static final String unrelevant = "unrelevant";
 
     @Autowired
     private TestEntityManager entityManager;
@@ -34,6 +37,7 @@ public class UserRepositoryTest {
     public void setUp() {
         user = new User();
         user.setFirstName("Jan");
+        user.setLastName("Nowak");
         user.setEmail("john@domain.com");
         user.setAccountStatus(AccountStatus.NEW);
     }
@@ -65,4 +69,87 @@ public class UserRepositoryTest {
         assertThat(persistedUser.getId(), notNullValue());
     }
 
+
+    @Test
+    public void shouldFindCorrectUserWithFirstName() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(user.getFirstName(),
+                unrelevant, unrelevant);
+        assertThat(users.size(), is(1));
+    }
+
+    @Test
+    public void shouldFindCorrectUserWithFirstNameUpperCase() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(user.getFirstName()
+                        .toUpperCase(),
+                unrelevant, unrelevant);
+        assertThat(users.size(), is(1));
+    }
+
+    @Test
+    public void shouldFindCorrectUserWithLastName() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(unrelevant,
+                user.getLastName(), unrelevant);
+        assertThat(users.size(), is(1));
+    }
+
+    @Test
+    public void shouldFindCorrectUserWithLastNameUpperCase() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(unrelevant,
+                user.getLastName()
+                        .toUpperCase(), unrelevant);
+        assertThat(users.size(), is(1));
+    }
+
+    @Test
+    public void shouldFindCorrectUserWithEmail() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(unrelevant,
+                unrelevant, user.getEmail());
+        assertThat(users.size(), is(1));
+    }
+
+    @Test
+    public void shouldFindCorrectUserWithEmailUpperCase() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(unrelevant,
+                unrelevant, user.getEmail()
+                        .toUpperCase());
+        assertThat(users.size(), is(1));
+    }
+
+    @Test
+    public void shouldNotFindIncorrectUser() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(unrelevant,
+                unrelevant, unrelevant);
+        assertThat(users.size(), is(0));
+    }
+
+    @Test
+    public void shouldNotFindIncorrectUserUpperCase() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(
+                unrelevant.toUpperCase(), unrelevant.toUpperCase(), unrelevant.toUpperCase());
+        assertThat(users.size(), is(0));
+    }
+
+    @Test
+    public void shouldFindCorrectUserWithFirstNameAndLastNameOnly() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(user.getFirstName(),
+                user.getLastName(), unrelevant);
+        assertThat(users.size(), is(1));
+    }
+
+    @Test
+    public void shouldFindCorrectUserWithFirstNameAndEmailOnly() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(user.getFirstName(),
+                unrelevant, user.getEmail());
+        assertThat(users.size(), is(1));
+    }
 }
