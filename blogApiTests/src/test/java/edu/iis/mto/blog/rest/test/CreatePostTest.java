6c9a5adc34a694b066164bf476e7f74235bdf4c9
liberadzkih <liberadzkih@gmail.com>
2020-09-15 19:32:34 +0200

    (Functional tests) Created CreatePostTest class layout

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
@@ -0,0 +1,19 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.junit.jupiter.api.Test;
+
+public class CreatePostTest extends FunctionalTests {
+
+    private static final String USER_POST_API = "/blog/user/{id}/post";
+
+    @Test
+    private void postShouldBeAddedWhenUserStatusIsConfirmed() {
+
+    }
+
+    @Test
+    private void postShouldNotBeAddedWhenUserStatusIsNotConfirmed() {
+        
+    }
+
+}
