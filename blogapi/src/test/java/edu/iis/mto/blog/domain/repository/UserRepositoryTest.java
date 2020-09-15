0041a54efada92fecf47355420ccd6cc1deb1937
liberadzkih <liberadzkih@gmail.com>
2020-09-15 03:40:02 +0200

    Implemented new UserRespositoryTest tests

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -8,12 +8,16 @@ import static org.junit.Assert.assertThat;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.iis.mto.blog.domain.model.AccountStatus;
@@ -35,34 +39,96 @@ public class UserRepositoryTest {
     public void setUp() {
         user = new User();
         user.setFirstName("Jan");
+        user.setLastName("Blachowicz");
         user.setEmail("john@domain.com");
         user.setAccountStatus(AccountStatus.NEW);
     }
-    
+
     @Test
     public void shouldFindNoUsersIfRepositoryIsEmpty() {
-
         List<User> users = repository.findAll();
-
         assertThat(users, hasSize(0));
     }
-    
+
     @Test
     public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
         User persistedUser = entityManager.persist(user);
         List<User> users = repository.findAll();
-
         assertThat(users, hasSize(1));
-        assertThat(users.get(0)
-                        .getEmail(),
-                equalTo(persistedUser.getEmail()));
+        assertThat(users.get(0).getEmail(), equalTo(persistedUser.getEmail()));
     }
-    
+
+    @Test
+    public void shouldFindOneUsersByUserFirstName() {
+        User persistedUser = entityManager.persist(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(user.getFirstName(), "",
+                "");
+        assertThat(users, hasSize(1));
+        assertThat(users.get(0), equalTo(persistedUser));
+    }
+
+    @Test
+    public void shouldFindOneUsersByUserLastName() {
+        User persistedUser = entityManager.persist(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("", user.getLastName(),
+                "");
+        assertThat(users, hasSize(1));
+        assertThat(users.get(0), equalTo(persistedUser));
+    }
+
+    @Test
+    public void shouldFindOneUsersByUserEmail() {
+        User persistedUser = entityManager.persist(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("", "", user.getEmail());
+        assertThat(users, hasSize(1));
+        assertThat(users.get(0), equalTo(persistedUser));
+    }
+
+    @Test
+    public void shouldFindOneThousandUsers() {
+        for (int i = 0; i <= 999; i++) {
+            user = new User();
+            user.setFirstName("JaASFSDn");
+            user.setLastName("ASAsASAsaDASF");
+            user.setEmail("sasdasdas" + i + "@easdasdas.pl");
+            user.setAccountStatus(AccountStatus.NEW);
+            repository.save(user);
+        }
+        List<User> users = repository.findAll();
+        assertThat(users, hasSize(1000));
+    }
+
+    @Test
+    public void shouldNotAllowToSaveSecondUserWithTheSameEmailAndThrowException() {
+        user = new User();
+        user.setFirstName("Adam");
+        user.setLastName("Malysz");
+        user.setEmail("skoki@narciarskie.pl");
+        user.setAccountStatus(AccountStatus.NEW);
+        repository.save(user);
+
+        user = new User();
+        user.setFirstName("Jane");
+        user.setLastName("Ahonen");
+        user.setEmail("skoki@narciarskie.pl");
+        user.setAccountStatus(AccountStatus.NEW);
+
+        assertThrows(DataIntegrityViolationException.class, () -> repository.save(user));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenNullIsFindMethodParameter() {
+        assertThrows(InvalidDataAccessApiUsageException.class,
+                () -> repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("", "", null));
+        assertThrows(InvalidDataAccessApiUsageException.class,
+                () -> repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("", null, ""));
+        assertThrows(InvalidDataAccessApiUsageException.class,
+                () -> repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(null, "", ""));
+    }
+
     @Test
     public void shouldStoreANewUser() {
-
         User persistedUser = repository.save(user);
-
         assertThat(persistedUser.getId(), notNullValue());
     }
 
